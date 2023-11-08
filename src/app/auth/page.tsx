036331a6051e2a910ece6fe809cdb607c6e6cf51
Daniel235-web <samueledaniel235@gmail.com>
2023-11-08 22:52:35 +0000

    myfirstcommit

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,23 @@
+import {AiFillGithub} from 'react-icons/ai';
+import {FcGoogle} from 'react-icons/fc';
+
+const Auth = () => {
+  return (
+    <section className="container mx-auto">
+      <div className="p-6 space-y-4 md:space-y-6 sm:p-8 w-80 md:w-[70%] mx-auto">
+        <div className="flex mb-8 flex-col md:flex-row items-center justify-between">
+            <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl "> 
+            create an account
+            </h1>
+            <p>OR</p>
+            <span className='inline-flex items-center'>
+                <AiFillGithub className='mr-3 text-4xl cursor-pointer text-black dark:text-white'/> 
+                <FcGoogle className='ml-3 text-4xl cursor-pointer'/>
+            </span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Auth;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Link from "next/link";
+import {useContext } from 'react'
+import {FaUserCircle} from "react-icons/fa";
+import { MdDarkMode, MdOutlineLightMode } from "react-icons/md";
+import ThemeContext from "@/context/themeContext";
+
+const Header = () => {
+  const  {darkTheme, setDarkTheme} = useContext (ThemeContext)
+  return (
+    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center">
+      <div className="flex  item-center w-full md:2/3">
+        <Link href="/" className="font-black text-tertiary-dark ">
+          Hotelzz
+        </Link>
+        <ul className="flex items-center ml-5">
+          <li className="flex items-center">
+            <Link href="/auth">
+              <FaUserCircle className="cursor-pointer" />
+            </Link>
+          </li>
+          <li className="ml-2">
+            {darkTheme ? (
+              <MdOutlineLightMode
+                className="cursor-pointer"
+                onClick={() => {
+                  setDarkTheme(false);
+                  localStorage.removeItem("hotel-theme");
+                }}
+              />
+            ) : (
+              <MdDarkMode
+                className="cursor-pointer"
+                onClick={() => {
+                  setDarkTheme(true);
+                  localStorage.setItem("hotel-theme", "true" );
+                }}
+              />
+            )}
+          </li>
+        </ul>
+      </div>
+
+      <ul className="flex items-center justify-between w-full md:w-1/3 mt-4 ">
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/rooms">Rooms</Link>
+        </li>
+        <li className="hover:-translate-y-2 duration-500 transition-all">
+          <Link href="/">Contact</Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
