66bf5f894ac08d72623fcdafa4787dcdf23159ba
Daniel235-web <samueledaniel235@gmail.com>
2023-11-18 11:54:35 +0000

    Create Booking API

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -1,29 +1,39 @@
-import Header from '@/components/Header/Header';
-import './globals.css'
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import Footer from '@/components/Footer/Footer';
-import ThemeProvider from '@/components/ThemeProvider/ThemeProvider';
-import { NextAuthProvider } from '@/components/AuthProvider/AuthProvider';
-import Toast from '@/components/Toast/Toast';
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '700', '900'], 
- style: ['italic' , 'normal'],
- variable: "--font-poppins"
- });
+import Header from "@/components/Header/Header";
+import "./globals.css";
+import Footer from "@/components/Footer/Footer";
+import ThemeProvider from "@/components/ThemeProvider/ThemeProvider";
+import { NextAuthProvider } from "@/components/AuthProvider/AuthProvider";
+import Toast from "@/components/Toast/Toast";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  style: ["italic", "normal"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
-  title: 'hotelmanagement',
-  description: 'Discover the best hotel rooms',
-}
+  title: "Hotel Management App",
+  description: "Discover the best hotel rooms",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={poppins.className}>
         <NextAuthProvider>
           <ThemeProvider>
--- src/libs/apis.ts
+++ src/libs/apis.ts
@@ -1,30 +1,71 @@
-import { Room } from "@/models/room";
-import sanityClient from "./sanity";
-import * as queries from "./sanityQueries"
-export  async function getFeaturedRoom() {
-    const result = await sanityClient.fetch<Room>(
-    queries.getFeaturedRoom, 
-        {}, 
-        {cache: "no-cache"}
-    );
+import axios from "axios";
 
-    return result;
+import { CreateBookingDto, Room } from "@/models/room";
+import sanityClient from "./sanity";
+import * as queries from "./sanityQueries";
+
+export async function getFeaturedRoom() {
+  const result = await sanityClient.fetch<Room>(
+    queries.getFeaturedRoom,
+    {},
+    { cache: "no-cache" }
+  );
+
+  return result;
 }
 
 export async function getRooms() {
-    const result = await sanityClient.fetch<Room[]>(
-      queries.getRoomsQuery,
-      {},
-      { cache: "no-cache" }
-    );
-    return result;
+  const result = await sanityClient.fetch<Room[]>(
+    queries.getRoomsQuery,
+    {},
+    { cache: "no-cache" }
+  );
+  return result;
 }
 
-export  async function getRoom(slug: string) {
-    const result = await sanityClient.fetch<Room>(
-      queries.getRoom,
-      { slug },
-      { cache: "no-cache" }
-    );
-return  result;
+export async function getRoom(slug: string) {
+  const result = await sanityClient.fetch<Room>(
+    queries.getRoom,
+    { slug },
+    { cache: "no-cache" }
+  );
+  return result;
 }
+
+export const createBooking = async ({
+  adults,
+  checkinDate,
+  checkoutDate,
+  hotelRoom,
+  children,
+  discount,
+  numberOfDays,
+  totalPrice,
+  user,
+}: CreateBookingDto) => {
+  const mutation = {
+    mutations: [
+      {
+        create: {
+          _type: "booking",
+          user: { _type: "reference", _ref: user },
+          hotelRoom: { _type: "reference", _ref: hotelRoom },
+          checkinDate,
+          checkoutDate,
+          numberOfDays,
+          adults,
+          children,
+          totalPrice,
+          discount,
+        },
+      },
+    ],
+  };
+
+  const { data } = await axios.post(
+    `https://${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}.api.sanity.io/v2021-10-21/data/mutate/${process.env.NEXT_PUBLIC_SANITY_DATASET}`,
+    mutation,
+    { headers: { Authorization: `Bearer ${process.env.SANITY_STUDIO_TOKEN}` } }
+  );
+  return data;
+};
