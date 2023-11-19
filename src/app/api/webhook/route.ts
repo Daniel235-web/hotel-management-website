39ec4541e84edb03f1c1d7388044d0c84d42b1c9
Daniel235-web <samueledaniel235@gmail.com>
2023-11-19 20:11:35 +0000

    stripe integration

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { createBooking, updateHotelRoom } from "@/libs/apis";
 
 const checkout_session_completed = "checkout.session.completed";
 
@@ -8,18 +9,68 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 });
 
 export async function POST(req: Request, res: Response) {
-    const reqBody = await req.text()
-    const sig = req.headers.get("stripe-signature");
-    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  const reqBody = await req.text();
+  const sig = req.headers.get("stripe-signature");
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
 
-    let event: Stripe.Event;
+  let event: Stripe.Event;
 
-    try {
-        if (!sig || !webhookSecret) return;
-        event = stripe.webhooks.constructEvent(reqBody, sig, webhookSecret);
-    } catch (error: any) {
-        return new NextResponse(`webhook Error: ${error.message}`, { status: 500 });
-    }
+  try {
+    if (!sig || !webhookSecret) return;
+    event = stripe.webhooks.constructEvent(reqBody, sig, webhookSecret);
+  } catch (error: any) {
+    return new NextResponse(`webhook Error: ${error.message}`, { status: 500 });
+  }
+  
+  //loading our event
 
+  switch (event.type) {
+    case checkout_session_completed:
+      const session = event.data.object;
+       const {
+        //@ts-ignore
+         metadata: {
+          adults,
+          checkinDate,
+          checkoutDate,
+          children,
+          hotelRoom,
+          numberOfDays,
+          user,
+          discount,
+          totalPrice
+
+         },
+       } = session
+
+      await createBooking({
+        adults: Number(adults), 
+        checkinDate,
+        checkoutDate,
+        children: Number(children),
+        hotelRoom,
+        numberOfDays:Number(numberOfDays),
+        discount: Number(discount),
+        totalPrice: Number(totalPrice),
+        user,
+
+      });
+      //Update hotel room
+       await updateHotelRoom(hotelRoom);
+       
+      return NextResponse.json("Booking successful!", {
+        status: 200,
+        statusText: "Booking successful",
+      });
+
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+   return NextResponse.json("Event Received", {
+     status: 200,
+     statusText: "Event Received",
+   });
 
 }
+
